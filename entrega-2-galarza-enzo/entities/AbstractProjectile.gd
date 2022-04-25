8c08f809cc4357185a7e1da2a240bf93d622592f
EnzoGalarza <EnzoGl33@hotmail.com>
2022-04-25 13:35:32 -0300

    Agrego timeout en el disparo del Cannon

--- entrega-2-galarza-enzo/entities/AbstractProjectile.gd
+++ entrega-2-galarza-enzo/entities/AbstractProjectile.gd
@@ -1,6 +1,10 @@
 extends Sprite
 class_name Projectile
 
+signal delete_requested(projectile)
+
+export (float) var speed
+
 var direction:Vector2
 
 func _ready():
@@ -9,7 +13,12 @@ func _ready():
 func set_starting_values(starting_position: Vector2, fire_direction : Vector2):
 	global_position = starting_position
 	self.direction = fire_direction
+	$Timer.start()
 	set_physics_process(true)
 
 func _physics_process(delta):
-	position += direction*50*delta
+	position += direction*speed*delta
+
+
+func _on_Timer_timeout():
+	emit_signal("delete_requested", self)
--- entrega-2-galarza-enzo/entities/player/Cannon.gd
+++ entrega-2-galarza-enzo/entities/player/Cannon.gd
@@ -11,6 +11,11 @@ func fire():
 	var real_fire_position = fire_position.global_position - global_position
 	projectile_container.add_child(projectile_instance)
 	projectile_instance.set_starting_values(fire_position.global_position,real_fire_position.normalized())
-
+	projectile_instance.connect("delete_requested", self, "_on_projectile_delete_requested")
+	
 func set_container(container : Node):
 	projectile_container = container	
+
+func _on_projectile_delete_requested(projectile):
+	projectile_container.remove_child(projectile)
+	projectile.queue_free()
