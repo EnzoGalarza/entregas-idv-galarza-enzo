5c89eb66f76636fc063726c9bd3d5d788853ebee
EnzoGalarza <EnzoGl33@hotmail.com>
2022-04-25 14:59:29 -0300

    Creo escena de torreta y comportamiento de disparo al player

--- entrega-2-galarza-enzo/entities/player/Player.gd
+++ entrega-2-galarza-enzo/entities/player/Player.gd
@@ -1,9 +1,13 @@
 extends Sprite
 
 onready var cannon : Sprite = $Cannon
-var projectile_container : Node
-var speed = 200
 
+export (float) var acceleration:float = 20.0
+export (float) var speed_limit:float = 600.0
+export (float) var friction_weight:float = 0.1
+
+var projectile_container : Node
+var velocity : Vector2 = Vector2.ZERO
 
 func set_projectile_container(container : Node):
 	cannon.set_container(container)
@@ -19,4 +23,8 @@ func _physics_process(delta):
 	if Input.is_action_just_pressed("fire"):
 		cannon.fire()
 	
-	position.x += direction_optimized * speed * delta
+	if direction_optimized != 0:
+		velocity.x	= clamp(velocity.x + (direction_optimized * acceleration), -speed_limit, speed_limit)
+	else:
+		velocity.x = lerp(velocity.x, 0, friction_weight) if abs(velocity.x) > 1 else 0	
+	position += velocity * delta
