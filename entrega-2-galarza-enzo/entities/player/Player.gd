74dcd51837ce8f5cf887573670aa9d1344356f63
EnzoGalarza <EnzoGl33@hotmail.com>
2022-04-25 11:17:37 -0300

    Importando proyecto entrega 2 y creando cannon

---
+++ entrega-2-galarza-enzo/entities/player/Player.gd
@@ -0,0 +1,16 @@
+extends Sprite
+
+onready var cannon : Sprite = $Cannon
+var speed = 200
+
+func _physics_process(delta):
+	
+	var direction_optimized:int = int(Input.is_action_pressed("move_right")) - int(Input.is_action_pressed("move_left"))
+	var mouse_position : Vector2 = get_global_mouse_position()
+	
+	cannon.look_at(mouse_position)
+	
+	if Input.is_action_just_pressed("fire"):
+		cannon.fire()
+	
+	position.x += direction_optimized * speed * delta
