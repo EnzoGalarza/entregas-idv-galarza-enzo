d7a73f84a73599ec8a8d6d9f42a0fb8644228204
EnzoGalarza <EnzoGl33@hotmail.com>
2022-05-04 13:20:51 -0300

    Fix bug de desaparicion de proyectil

--- entrega-3-galarza-enzo/entities/enemies/TurretsSpawner.gd
+++ entrega-3-galarza-enzo/entities/enemies/TurretsSpawner.gd
@@ -7,9 +7,10 @@ func _ready():
 	call_deferred("_initialize")
 
 func _initialize():
+	var view:Rect2 = get_viewport().get_visible_rect()
 	for i in 3:
 		var turret_instance = turret_scene.instance()
 		
-		var turret_pos:Vector2 = Vector2(rand_range(position.x, position.x + lenght.x), rand_range(position.y, position.y + lenght.y))
+		var turret_pos:Vector2 = Vector2(rand_range(view.position.x, view.end.x), rand_range(view.position.y, view.end.y))
 		
 		turret_instance.initialize(self, turret_pos, self)
